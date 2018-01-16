bfc9aec34bac46527d7e4fa09089310c51dec1e9
lijianghao <337990443@qq.com>
2018-01-16 20:08:55 +0800

    add 'None' condition

--- src/flatbuffers/flatbuffer_helper.rs
+++ src/flatbuffers/flatbuffer_helper.rs
@@ -21,7 +21,14 @@ pub struct FlatBufferHelper {
 // 全是基本类型的转化
 // 对 整个data做转化，不考虑类型是否匹配
 impl FlatBufferHelper {
-    pub fn from_bool(&self, data: bool) -> Option<FlatBufferVar> {
+    pub fn from_option_bool(&self, data: Option<bool>) -> Option<FlatBufferVar> {
+        let data = match data {
+            Some(data) => data,
+            None => return None,
+        };
+        self.from_bool(data)
+    }
+    fn from_bool(&self, data: bool) -> Option<FlatBufferVar> {
         let mut data_vec = vec![0u8;1];
         if data {
             data_vec = vec![1u8;1];
@@ -29,16 +36,28 @@ impl FlatBufferHelper {
         FlatBufferVar::new( FlatBufferType::FBBool, 
                             Some(data_vec), None, None, None )
     }
-    pub fn from_string(&self, data: String) -> Option<FlatBufferVar> {
+    pub fn from_option_string(&self, data: Option<String>) -> Option<FlatBufferVar> {
+        let data = match data {
+            Some(data) => data,
+            None => return None,
+        };
+        self.from_string(data)
+    }
+    fn from_string(&self, data: String) -> Option<FlatBufferVar> {
         let mut data_vec = data.into_bytes();
         FlatBufferVar::new( FlatBufferType::FBString, 
                             Some(data_vec), None, None, None )
     }
     // rust 原始数据，不做任何改动 转为对应的FlatBufferVar
+    pub fn from_option_vec<T>(&self, data: Option<Vec<T>>, e_type: u8) -> Option<FlatBufferVar> where T: Any {
+        let data = match data {
+            Some(data) => data,
+            None => return None,
+        };
+        self.from_vec(&data, e_type)
+    }
     // T 只能是 String bool FlatBufferVar 其中一种
-    pub fn from_vec<T>( &self, data:&Vec<T>, e_type: u8) 
-                        -> Option<FlatBufferVar> where T: Any
-    {
+    pub fn from_vec<T>( &self, data:&Vec<T>, e_type: u8) -> Option<FlatBufferVar> where T: Any {
         // 通过反射机制，得到T的类型  或者 增加参数，告知传入的类型
         let mut flatbuffer_list = Vec::new();
         if e_type == 0u8 {
@@ -55,14 +74,22 @@ impl FlatBufferHelper {
             }
         } else if e_type == 1u8 {
             // string
+            // println!("vec<string> len is {}", data.len());
             for len in 0..data.len() {
                 // let element = data[len];
                 let value_any = &data[len] as &Any;
                 let value_string = match value_any.downcast_ref::<String>() {
-                    Some(as_string) => as_string.as_str().to_string(),
-                    None => continue, // 可能出错了
+                    Some(as_string) => {
+                        // println!("string is {:?}", as_string);
+                        as_string.as_str().to_string()
+                    },
+                    None => {
+                        println!("没有转换成功");
+                        continue;
+                    }, // 可能出错了
                 };
                 flatbuffer_list.push(self.from_string(value_string).unwrap());
+                // println!("增加{} 的list: {:?}", value_string, flatbuffer_list);
             }
         } else if e_type == 2u8 {
             // list struct 合并
@@ -144,15 +171,39 @@ impl FlatBufferHelper {
     // }
 
     // 可能不是特别好用 数据可以再进行操作
-    pub fn from_flatbuffer(&self, data:&mut FlatBuffer) -> Option<FlatBufferVar> {
+    pub fn from_option_flatbuffer(&self, data: Option<FlatBuffer>) -> Option<FlatBufferVar> {
+        let mut data = match data {
+            Some(data) => data,
+            None => return None,
+        };
+        self.from_flatbuffer(&mut data)
+    }
+    fn from_flatbuffer(&self, data:&mut FlatBuffer) -> Option<FlatBufferVar> {
         let (bytes, pivot) = data.bytes();
         // println!("本次bytes is {:?}", bytes);
         FlatBufferVar::new( FlatBufferType::FBStruct, 
                             bytes, None, None, Some(pivot as u32) )
     }
+    pub fn to_option_string(&self, data: Option<Vec<u8>>, root: usize) -> Option<String> {
+        let data = match data {
+            Some(s) => s,
+            None => return None,
+        };
+        let value = self.to_string(&data, root);
+        Some(value)
+    }
+    
     pub fn to_string(&self, data: &Vec<u8>, root: usize) -> String {
         let end = data.len();
         String::from_utf8(data[root..end].to_vec()).unwrap()
+    }
+    pub fn to_option_bool(&self, data: Option<Vec<u8>>, root: usize) -> Option<bool> {
+        let data = match data {
+            Some(s) => s,
+            None => return None,
+        };
+        let value = self.to_bool(&data, root);
+        Some(value)
     }
     pub fn to_bool(&self, data: &Vec<u8>, root: usize) -> bool {
         // 错误处理可以更好
@@ -163,7 +214,7 @@ impl FlatBufferHelper {
         }
     }
     // 专门为数组准备的 数组中不存在None
-    fn to_vec(&self, data: &Vec<u8>, root: usize) -> Result<Vec<Vec<u8>>, String> {
+    pub fn to_vec(&self, data: &Vec<u8>, root: usize) -> Result<Vec<Vec<u8>>, String> {
         let handler = FlatBufferHandler{};
         let mut bytes_list = Vec::new();
 
@@ -190,6 +241,7 @@ impl FlatBufferHelper {
         }
         Ok(string_list)
     }
+    // pub fn to_option_struct(&self, data:Option<>)
     // 所有字段分离出来，存入Vec 字段可能为空
     pub fn to_struct(&self, data: &Vec<u8>, root: usize) -> Result<Vec<Option<Vec<u8>>>, String> {
         let handler = FlatBufferHandler{};
